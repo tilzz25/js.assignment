169a1d393539587594b5ea1906d2583338eeb613
tilzz25 <tilakp1151@gmail.com>
2025-01-27 16:29:06 +0300

    qn2.js

---
+++ qn2.js
@@ -0,0 +1,14 @@
+const person = {
+    firstname: 'tilak',
+    lastname: 'patel',
+    fullname: function() {
+        return this.firstname + '' + this.lastname;
+    }
+    };
+
+//How many properties does the object have? Name them
+//Answer -
+ //3 Properties
+ //The firstname
+ //The lastname
+ //The fullname

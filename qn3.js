3920e2404b5f947c07f6ca695e8f3ef3002ce9ab
tilzz25 <tilakp1151@gmail.com>
2025-01-27 16:31:26 +0300

    answered qn 3

---
+++ qn3.js
@@ -0,0 +1,12 @@
+const person = {
+    firstname: 'ansh',
+    lastname: 'patel',
+    fullname: function() {
+      return this.firstname + ' ' + this.lastname;
+    }
+  };
+  //How many methods does the object have?, Name them?
+  //Answer -
+  //1 Method
+  //The fullname method
+  

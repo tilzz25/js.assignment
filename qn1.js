4b22a6f0fd4074efa0497e3ef9ea277e5a8595b9
tilzz25 <tilakp1151@gmail.com>
2025-01-27 16:27:18 +0300

    qn1.js

---
+++ qn1.js
@@ -0,0 +1,14 @@
+const car = {
+    brand : 'BMW',
+    model : 'TOYOTA'
+
+};
+
+//There are two correct syntax to alert BMW, name them
+
+//1) The bracket notation
+console.log(car['brand']);
+
+
+//2) THe dot notation
+console.log(car.brand);
